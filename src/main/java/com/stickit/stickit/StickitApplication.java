df4d26be610ed5347513e2ea6a701a427e0f6e3e
David Ericson <davidestudofcg@gmail.com>
2026-01-26 06:48:04 -0300

    feat: Spring Boot project setup

---
+++ src/main/java/com/stickit/stickit/StickitApplication.java
@@ -0,0 +1,13 @@
+package com.stickit.stickit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stickit/stickit/StickitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stickit.stickit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
